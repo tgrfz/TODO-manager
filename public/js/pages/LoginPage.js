710a77056fd693c21c0aec87add46a87acd6b50f
WhiteTigerrrrr <46075222+WhiteTigerrrrr@users.noreply.github.com>
2021-05-16 18:23:00 +0300

    add authentication

--- public/js/pages/LoginPage.js
+++ public/js/pages/LoginPage.js
@@ -1,19 +1,19 @@
 import navigate from "../../app.js";
-// import {ifLoggedIn, login} from "../users.js";
+import {ifLoggedIn, login} from "../users.js";
 import {onClick, onSubmit} from "../utils.js";
 
 let LoginPage = {
     before_render: async () => {
-        // ifLoggedIn(() => navigate("/boards"));
+        ifLoggedIn(() => navigate("/boards"));
     },
     render: async () => {
         return `
             <div class="login-wrap">
                 <div class="login-block">
                     <h2>Log in</h2>
-                    <form id="login_form" class="login-form" method="post">
-                        <input type="text" id="login" name="login" placeholder="Login" minlength="5" required/>
-                        <input type="Password" id="password" name="password" placeholder="Password" minlength="8" required/>
+                    <form id="login_form" class="login-form">
+                        <input type="email" id="email" name="email" placeholder="Login" minlength="5" required/>
+                        <input type="password" id="password" name="password" placeholder="Password" minlength="8" required/>
                         <button class="button-like" type="submit" >Log in</button>
                     </form>
                     <a id="sign_up" href="/signup">Sign up for an account</a>
@@ -22,9 +22,9 @@ let LoginPage = {
         `;
     },
     after_render: async () => {
-        // onSubmit(document.getElementById("login_form"), ({email, password}) => {
-        //     login(email, password).then(() => navigate("/boards"))
-        // });
+        onSubmit(document.getElementById("login_form"), ({email, password}) => {
+            login(email, password).then(() => navigate("/boards"))
+        });
 
         onClick(document.getElementById("sign_up"), () => {
             navigate("/signup")
--- public/js/pages/SignupPage.js
+++ public/js/pages/SignupPage.js
@@ -1,20 +1,19 @@
 import navigate from "../../app.js";
-// import {ifLoggedIn, signup} from "../users.js";
+import {ifLoggedIn, signup} from "../users.js";
 import {onClick, onSubmit} from "../utils.js";
 
 let SignupPage = {
     before_render: async () => {
-        // ifLoggedIn(() => navigate("/boards"));
+        ifLoggedIn(() => navigate("/boards"));
     },
     render: async () => {
         return `
             <div class="login-wrap">
                 <div class="login-block">
                     <h2>Sign up</h2>
-                    <form id="login_form" class="login-form" method="post">
-                        <input type="text" id="login" name="login" placeholder="Login" minlength="5" required/>
-                        <input type="Password" id="password" name="password" placeholder="Password" minlength="8" required/>
-                        <input type="Password" id="password2" name="password2" placeholder="Repeat password" minlength="8" required/>
+                    <form id="login_form" class="login-form">
+                        <input type="email" id="email" name="email" placeholder="Login" minlength="5" required/>
+                        <input type="password" id="password" name="password" placeholder="Password" minlength="8" required/>
                         <button class="button-like" type="submit">Sign up</button>
                     </form>
                     <a id="log_in" href="/login">Already have an account? Log In</a>
@@ -23,9 +22,9 @@ let SignupPage = {
         `;
     },
     after_render: async () => {
-        // onSubmit(document.getElementById("login_form"), ({email, password}) => {
-        //     signup(email, password).then(() => navigate("/boards"));
-        // });
+        onSubmit(document.getElementById("login_form"), ({email, password}) => {
+            signup(email, password).then(() => navigate("/boards"));
+        });
 
         onClick(document.getElementById("log_in"), () => {
             navigate("/login")
